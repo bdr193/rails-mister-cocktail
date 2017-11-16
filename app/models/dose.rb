a09dc525ee02e963e55c473c7e95c0cc2964151e
rabea bader <bdr193@gmail.com>
2017-11-16 12:25:36 +0100

    rake works

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,10 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
+  validates :ingredient, presence: true
+  # validates :cocktail, uniqueness: { scope: :ingredient, case_sensitive: false, message: “You can’t repeat an ingredient for the same cocktail!” }
+
 end
